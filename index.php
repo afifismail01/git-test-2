dccd95f1807ff843a7b7b65e130cfce195893561
afif_ismail01 <afifismail021@gmail.com>
2024-09-01 22:00:27 +0700

    mengganti file index.html dengan index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam non ipsum corrupti, est quasi illo veritatis reprehenderit voluptatibus inventore accusamus assumenda perferendis suscipit vero repudiandae officia sapiente accusantium delectus eos voluptas labore sequi voluptatum dicta iure. Ab vero architecto repudiandae assumenda, hic similique est, libero dolores ex nihil aut! Est sapiente qui in perferendis, temporibus mollitia eum tenetur incidunt sunt fugiat amet laboriosam ratione consectetur. Consectetur neque voluptatibus sapiente, adipisci saepe reprehenderit perspiciatis. Laborum, ducimus distinctio consequuntur aliquam voluptas perspiciatis qui nemo dolorem, et quidem soluta sit? Obcaecati explicabo architecto possimus quo autem, eveniet, et quas eaque iure, quia aliquam.</p>
+</body>
+</html>
